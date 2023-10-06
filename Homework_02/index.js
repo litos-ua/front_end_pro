65ff7709b1b8b5d301574279f6f59d9ab9a0c1c4
litos-ua <andrzejlitkiewicz@gmail.com>
2023-10-06 15:47:02 +0300

    Homework_2.04 & Homework_3.04

--- Homework_02/index.js
+++ Homework_02/index.js
@@ -1,25 +1,15 @@
-let firstNumber, secondNumber;
+let firstNumber, secondNumber, resultOut;
 
 firstNumber = Number(prompt("Enter the first number.",'0'));
 secondNumber = Number(prompt("Enter the second number.",'1'));
+resultOut = `Calculations has finished!\n
+       sumCalc: ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber};\n
+       diffCalc: ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber};\n
+       multCalc: ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber};\n
+       divCalc: ${firstNumber} / ${secondNumber} = ${(firstNumber / secondNumber).toFixed(2)}`;
 
-alert(`Calculations has finished!\n
-       sumCalc: ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}\n
-       diffCalc: ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}\n
-       multCalc: ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}\n
-       divCalc: ${firstNumber} / ${secondNumber} = ${(firstNumber / secondNumber).toFixed(2)}`);
-
-console.log(`Calculations has finished!\n
-       sumCalc: ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}\n
-       diffCalc: ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}\n
-       multCalc: ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}\n
-       divCalc: ${firstNumber} / ${secondNumber} = ${(firstNumber / secondNumber).toFixed(2)}`);
-
-document.getElementById('display1').innerHTML =
-    `Calculations has finished!\n<br>
-    sumCalc: ${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}<br>
-    diffCalc: ${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}<br>
-    multCalc: ${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}<br>
-    divCalc: ${firstNumber} / ${secondNumber} = ${(firstNumber / secondNumber).toFixed(2)}`;
+alert(`${resultOut}`);
+console.log(`${resultOut}`);
+document.getElementById('display1').innerHTML = `${resultOut}`;
 
 
