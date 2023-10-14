b21ed168466d8adffed34921749e9175f91a13f0
litos-ua <andrzejlitkiewicz@gmail.com>
2023-10-14 23:41:33 +0300

    Homework_05.01 AND Homework_06.00

---
+++ Homework_06/index.js
@@ -0,0 +1,69 @@
+'use strict'
+
+alert('Welcome to calculator!');
+
+let action, action1, action2, a;
+let kindOfAction = prompt('What action you want to do? Add, Diff, Mult, Div, Sqrt, Exp, Sin; Cos');
+(kindOfAction === 'Add' || kindOfAction === 'Diff' || kindOfAction === 'Mult' || kindOfAction === 'Div') ? action1 =1 : action1 = 0;
+(kindOfAction === 'Sqrt' || kindOfAction === 'Exp' || kindOfAction === 'Sin' || kindOfAction === 'Cos') ? action2 =2 : action2 = 0;
+let firstNumberStr,  firstNumber, secondNumberStr, secondNumber, condNum1 = true, incorrectAct = true;
+
+action = action1 + action2;
+switch (action){
+    case 1:
+        firstNumberStr = prompt('Enter the first number.');
+        firstNumber = Number(firstNumberStr);
+        ((firstNumberStr !== null) && (!isNaN(firstNumber)) && (firstNumberStr !== '')) ? condNum1 = true: condNum1 = false;
+        secondNumberStr = prompt('Enter the second number.');
+        secondNumber = Number(secondNumberStr);
+        ((secondNumberStr !== null) && (!isNaN(secondNumber)) && (secondNumberStr !== '' && condNum1)) ? condNum1 = true: condNum1 = false;
+        break
+    case 2:
+        firstNumberStr = prompt('Enter the number.');
+        firstNumber = Number(firstNumberStr);
+        ((firstNumberStr !== null) && (!isNaN(firstNumber)) && (firstNumberStr !== '')) ? condNum1 = true: condNum1 = false;
+        break
+    default:
+        incorrectAct = false;
+}
+
+(!condNum1 && incorrectAct) ? kindOfAction = 'WNum': a = 'Nothing to do';
+
+
+switch (kindOfAction) {
+    case 'Add':
+        alert(`Sum of ${firstNumber} and ${secondNumber} is ${firstNumber + secondNumber}`);
+        break
+    case 'Diff':
+        alert(`Difference of ${firstNumber} and ${secondNumber} is ${firstNumber - secondNumber}`);
+        break
+    case 'Mult':
+        alert(`Product of ${firstNumber} and ${secondNumber} is ${firstNumber * secondNumber}`);
+        break
+    case  'Div':
+        secondNumber === 0 ? alert('Division by 0 is not possible. Good by, see you later.') :
+        alert(`Division of ${firstNumber} by ${secondNumber} is ${(firstNumber / secondNumber).toFixed(2)}`);
+        break
+    case "Sqrt":
+        firstNumber <= 0 ? alert('Extracting the square root of a number is not possible. Good by, see you later.') :
+        alert(`Square root of a number ${firstNumber} is ${Math.sqrt(firstNumber).toFixed(2)}`);
+        break
+    case 'Exp':
+        firstNumber <= 0 ? alert('Raising to a negative power is impossible. Good by, see you later.') :
+        alert(`Exponent of a number ${firstNumber} is ${Math.exp(firstNumber).toFixed(2)}`);
+        break
+    case 'Sin':
+        alert(`Sine of angle ${firstNumber} is ${Math.sin(firstNumber).toFixed(2)}`);
+        break
+    case  'Cos':
+        alert(`Cosine of angle ${firstNumber} is ${Math.cos(firstNumber).toFixed(2)}`);
+        break
+    case 'WNum':
+        alert('You entered an INCORRECT NUMBER, the program will stop working! Good by, see you later.');
+        break
+    default: alert('You entered an INCORRECT ACTION, the program will stop working! Good by, see you later.');
+}
+
+
+
+
