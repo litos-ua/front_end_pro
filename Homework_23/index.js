cbb4f9e5d6fb512249d01123ee5d2f38436838af
litos-ua <andrzejlitkiewicz@gmail.com>
2023-12-10 16:24:18 +0200

    Homework_23.05 Voting

--- Homework_23/index.js
+++ Homework_23/index.js
@@ -1,86 +1,5 @@
 'use strict';
 
-//     const counters = {
-//         btnVeryBad: 0,
-//         btnPoor: 0,
-//         btnSatisfactory: 0,
-//         btnGood: 0,
-//         btnExcellent: 0,
-//     };
-//
-//     function updateCounterDisplay(buttonId) {
-//         const countElement = document.getElementById(`count${buttonId}`);
-//         if (countElement) {
-//             countElement.textContent = counters[buttonId].toString();
-//         }
-//     }
-//
-//     function handleButClickCount(buttonId) {
-//         counters[buttonId]++;
-//         console.log(`${buttonId}: ${counters[buttonId]}`);
-//         updateCounterDisplay(buttonId);
-//     }
-//
-//
-//     const tableBody = document.getElementById('counterTableBody');
-//     const tableHead = document.getElementById('counterTableHead');
-//
-//     const headerRow = document.createElement('tr');
-//     const headerCell = document.createElement('th');
-//     headerCell.textContent = 'Button';
-//     headerCell.style.cssText = 'text-align: center; font-size: 20px;'
-//     headerRow.append(headerCell);
-//
-//
-//     for (const buttonId in counters) {
-//         const headerCell = document.createElement('th');
-//         headerCell.style.textAlign = 'center';
-//         const img = document.createElement('img');
-//         img.src = `./images/${buttonId}.jpg`;
-//         img.alt = buttonId;
-//         headerCell.append(img);
-//         headerRow.append(headerCell);
-//     }
-//
-// tableHead.append(headerRow);
-//
-//     const countRow = document.createElement('tr');
-//     const countHeaderCell = document.createElement('th');
-//     countHeaderCell.textContent = 'Count';
-//     countHeaderCell.style.cssText = 'text-align: center; font-size: 20px;';
-//     countRow.append(countHeaderCell);
-//
-//     for (const buttonId in counters) {
-//         const countCell = document.createElement('td');
-//         countCell.id = `count${buttonId}`;
-//         countCell.textContent = counters[buttonId];
-//         countCell.style.cssText = 'text-align: center; font-size:18px;';
-//         countRow.append(countCell);
-//     }
-//     tableBody.append(countRow);
-//
-//
-//
-//
-// document.body.style.backgroundColor = 'lightblue';
-//     document.getElementById('btnVeryBad').addEventListener('click',
-//         () => handleButClickCount('btnVeryBad'));
-//     document.getElementById('btnPoor').addEventListener('click',
-//         () => handleButClickCount('btnPoor'));
-//     document.getElementById('btnSatisfactory').addEventListener('click',
-//         () => handleButClickCount('btnSatisfactory'));
-//     document.getElementById('btnGood').addEventListener('click',
-//         () => handleButClickCount('btnGood'));
-//     document.getElementById('btnExcellent').addEventListener('click',
-//         () => handleButClickCount('btnExcellent'));
-//     document.getElementById('btnReset').addEventListener('click', () => {
-//         for (const buttonId in counters) {
-//             counters[buttonId] = 0;
-//             updateCounterDisplay(buttonId);
-//         }
-//         console.log('Counters reset');
-//     });
-
 const counters = {
     btnVeryBad: 0,
     btnPoor: 0,
@@ -113,10 +32,37 @@ function handleResetButtonClick() {
     console.log('Counters reset');
 }
 
-const tableBody = document.getElementById('counterTableBody');
-const tableHead = document.getElementById('counterTableHead');
+const btnDiv = document.createElement('div');
+btnDiv.classList.add('d-flex', 'gap-2', 'justify-content-center', 'py-5');
+btnDiv.id = 'btnDiv';
 
+const buttonData = [
+    { id: 'btnVeryBad', text: 'Very bad', style: 'btn-primary' },
+    { id: 'btnPoor', text: 'Poor', style: 'btn-success' },
+    { id: 'btnSatisfactory', text: 'Satisfactory', style: 'btn-info' },
+    { id: 'btnGood', text: 'Good', style: 'btn-warning' },
+    { id: 'btnExcellent', text: 'Excellent', style: 'btn-danger' },
+    { id: 'btnReset', text: 'Reset', style: 'btn-light' },
+];
+
+buttonData.forEach(buttonInfo => {
+    const button = document.createElement('button');
+    button.classList.add('btn', `rounded-pill`, 'px-3', buttonInfo.style);
+    button.type = 'button';
+    button.id = buttonInfo.id;
+    button.textContent = buttonInfo.text;
+    btnDiv.append(button);
+});
+
+document.body.append(btnDiv);
+
+
+const table = document.createElement('table');
+table.classList.add('table');
+
+const tableHead = document.createElement('thead');
 const headerRow = document.createElement('tr');
+
 const headerCell = document.createElement('th');
 headerCell.textContent = 'Button';
 headerCell.style.cssText = 'text-align: center; font-size: 20px;';
@@ -134,6 +80,7 @@ for (const buttonId in counters) {
 
 tableHead.append(headerRow);
 
+const tableBody = document.createElement('tbody');
 const countRow = document.createElement('tr');
 const countHeaderCell = document.createElement('th');
 countHeaderCell.textContent = 'Count';
@@ -149,10 +96,12 @@ for (const buttonId in counters) {
 }
 
 tableBody.append(countRow);
+table.append(tableHead, tableBody);
+document.body.append(table);
 
 document.body.style.backgroundColor = 'lightblue';
 
-const btnDiv = document.getElementById('btnDiv');
+//const btnDiv = document.getElementById('btnDiv');
 btnDiv.addEventListener('click', handleVoteButtonClick);
 
 const btnReset = document.getElementById('btnReset');
