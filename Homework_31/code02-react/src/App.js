e0afb22188fd319b59e64fe6e59fee0c976addc9
litos-ua <andrzejlitkiewicz@gmail.com>
2024-01-18 20:34:57 +0200

    Homework_31.00 React.First experience

---
+++ Homework_31/code02-react/src/App.js
@@ -0,0 +1,45 @@
+
+import React from 'react';
+import './App.css';
+import './styles.css'
+
+const App = () => {
+  const numOfPictures = 9;
+
+  const createImage = (num) => {
+    return (
+        <div className="col-md-4" key={num}>
+          <div className="card" style={{ height: '100%' }}>
+            <img src={`./images/${num}.jpg`} className="card-img-top" alt={`Image ${num}`} style={{ objectFit: 'cover', height: '200px' }} />
+            <div className="card-body" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', backgroundColor: '#dded80' }}>
+              <h5 className="card-title">Image {num}</h5>
+              <p className="card-text"></p>
+            </div>
+          </div>
+        </div>
+    );
+  };
+
+  const getRandomImageNumber = (min, max) => {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  };
+
+  return (
+      // <div className="text-center">
+      //   <h2>Lessons_18</h2>
+      //   <h3>Homework_22: Images output</h3>
+        <div id="imageContainer" className="container mt-5">
+          <div className="row">
+            {[...Array(numOfPictures)].map(() => (
+                createImage(getRandomImageNumber(1, numOfPictures))
+            ))}
+          </div>
+        </div>
+      // </div>
+  );
+};
+
+export default App;
+
