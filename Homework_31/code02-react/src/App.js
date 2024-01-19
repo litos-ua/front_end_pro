dc63731b9ad936e55d523309a26e3f10ab126a27
litos-ua <andrzejlitkiewicz@gmail.com>
2024-01-19 16:29:36 +0200

    Homework_31.01 React.First experience

--- Homework_31/code02-react/src/App.js
+++ Homework_31/code02-react/src/App.js
@@ -10,8 +10,10 @@ const App = () => {
     return (
         <div className="col-md-4" key={num}>
           <div className="card" style={{ height: '100%' }}>
-            <img src={`./images/${num}.jpg`} className="card-img-top" alt={`Image ${num}`} style={{ objectFit: 'cover', height: '200px' }} />
-            <div className="card-body" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', height: '100%', backgroundColor: '#dded80' }}>
+            <img src={`./images/${num}.jpg`} className="card-img-top" alt={`Image ${num}`}
+                 style={{ objectFit: 'cover', height: '200px' }} />
+            <div className="card-body" style={{ display: 'flex', flexDirection: 'column', justifyContent: 'center',
+                                                height: '100%', backgroundColor: '#dded80' }}>
               <h5 className="card-title">Image {num}</h5>
               <p className="card-text"></p>
             </div>
@@ -27,9 +29,15 @@ const App = () => {
   };
 
   return (
-      // <div className="text-center">
-      //   <h2>Lessons_18</h2>
-      //   <h3>Homework_22: Images output</h3>
+      <div className="text-center">
+          <h2>Lessons_18</h2>
+          <div className="button-container" style={{ display: 'flex', alignItems: 'center',
+              justifyContent: 'space-between',paddingRight: '5%', paddingLeft: '5%'  }}>
+              <h3>Homework_22: Images output</h3>
+              <button className="btn btn-primary" style={{ marginLeft: '10px' }}>
+                  Restart
+              </button>
+          </div>
         <div id="imageContainer" className="container mt-5">
           <div className="row">
             {[...Array(numOfPictures)].map(() => (
@@ -37,7 +45,7 @@ const App = () => {
             ))}
           </div>
         </div>
-      // </div>
+      </div>
   );
 };
 
