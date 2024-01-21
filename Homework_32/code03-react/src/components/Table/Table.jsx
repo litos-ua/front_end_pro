c78555c5af763b4e2714cd98ef82eb157d10c141
litos-ua <andrzejlitkiewicz@gmail.com>
2024-01-21 23:52:53 +0200

    Homework_32.02 React.Contact table

--- Homework_32/code03-react/src/components/Table/Table.jsx
+++ Homework_32/code03-react/src/components/Table/Table.jsx
@@ -1,14 +1,38 @@
+'use strict'
 
 import React from 'react';
 import './Table.css';
 
+const tableClass = 'custom-table';
+const buttonPrimaryClass = 'btn btn-sm btn-primary me-1';
+const buttonDangerClass = 'btn btn-sm btn-danger';
 
 class Table extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.handleCreateUser = this.handleCreateUser(this);
+        this.handleEditUser = this.handleEditUser.bind(this);
+        this.handleDeleteUser = this.handleDeleteUser.bind(this)
+    }
+
+    handleCreateUser = (userId) => {
+        // Create
+    }
+
+    handleEditUser = (userId) => {
+        // Update
+    }
+
+    handleDeleteUser = (userId) => {
+        // Delete
+    }
+
     render() {
         const { users } = this.props;
 
         return (
-            <table className="custom-table">
+            <table className={tableClass}>
                 <thead>
                 <tr>
                     <th>ID</th>
@@ -26,12 +50,16 @@ class Table extends React.Component {
                         <td>{user.username}</td>
                         <td>{user.phone}</td>
                         <td>
-                            <button className="btn btn-sm btn-primary me-1" onClick={
+                            <button className={buttonPrimaryClass} onClick={
                                 () => console.log('Edit', user.id)
-                            }>Edit</button>
-                            <button className="btn btn-sm btn-danger" onClick={
+                            }>
+                                Edit
+                            </button>
+                            <button className={buttonDangerClass} onClick={
                                 () => console.log('Delete', user.id)
-                            }>Delete</button>
+                            }>
+                                Delete
+                            </button>
                         </td>
                     </tr>
                 ))}
@@ -40,7 +68,6 @@ class Table extends React.Component {
         );
     }
 }
-
 
 
 export { Table };
